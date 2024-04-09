085cb7054a3b29dcf59ea0e73011bcfd7860dce2
Payne81 <3334248237@qq.com>
2024-04-09 15:27:46 +0800

    delete go file

--- data/log_record.go
+++
@@ -1,9 +0,0 @@
-package data
-
-
-// Index impl: log record pos
-// position define by file id and offset
-type LogRecordPos struct {
-	Fid uint32 	
-	Offset int64 
-}
--- go.mod
+++
@@ -1,3 +0,0 @@
-module bottledb
-
-go 1.22.1
